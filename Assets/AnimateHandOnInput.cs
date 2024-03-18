074f825f568e8134cfec3e58ae1b99e9e4daec19
momouang <syes0923momo@gmail.com>
2024-03-18 00:28:32 +0000

    Revert "03/18"
    This reverts commit bbb20fb8d213b46484af2e5630fb4bf3f3b1152c.

--- Assets/AnimateHandOnInput.cs
+++ Assets/AnimateHandOnInput.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-<<<<<<< Updated upstream:Assets/AnimateHandOnInput.cs
 
 public class AnimateHandOnInput : MonoBehaviour
 {
@@ -15,23 +14,5 @@ public class AnimateHandOnInput : MonoBehaviour
     void Update()
     {
         
-=======
-using UnityEngine.InputSystem;
-
-public class AnimateHandOnInput : MonoBehaviour
-{
-    public InputActionProperty pinchAnimationAction;
-    public InputActionProperty gripAnimationAction;
-
-    public Animator handAnimator;
-
-    void Update()
-    {
-        float triggerValue = pinchAnimationAction.action.ReadValue<float>();
-        handAnimator.SetFloat("Trigger", triggerValue);
-
-        float gripValue = gripAnimationAction.action.ReadValue<float>();
-        handAnimator.SetFloat("Grip", gripValue);
->>>>>>> Stashed changes:Assets/Scripts/AnimateHandOnInput.cs
     }
 }
